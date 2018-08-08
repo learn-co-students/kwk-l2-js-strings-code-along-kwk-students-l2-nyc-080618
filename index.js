b58d8992c86653bd63d47dcb976a37aa182347d8
aishachoudery <aichoudery31@gmail.com>
2018-08-08 14:35:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+const currentUser = 'Aisha';
+const welcomeMessage = "Welcome to Klossybook, Aisha!";
+const excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, AISHA!";
+const shortGreeting = "Welcome, A!"
