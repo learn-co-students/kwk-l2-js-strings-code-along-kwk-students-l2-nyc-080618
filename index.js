685e76d0782fb58d56992a182c579cb759f18319
kyarato <kyarasto@gmail.com>
2018-08-08 14:36:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your code in this file!
 
+const currentUser = 'Kyara';
+const welcomeMessage = 'Welcome to Klossybook, Kyara!';
+
+const excitedWelcomeMessage = 'WELCOME TO KLOSSYBOOK, KYARA!'
+
+const shortGreeting = 'Welcome, K!'
