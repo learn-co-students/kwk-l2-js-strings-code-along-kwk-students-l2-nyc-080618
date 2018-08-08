e4fa6a84c86d3c00ee858e7973ae03137955cd2b
Claire Xu <claire02.xu@gmail.com>
2018-08-08 14:18:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Claire Xu';
 
+const welcomeMessage = `Welcome to Klossybook, + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, + ${currentUser.charAt(0)}!`;
